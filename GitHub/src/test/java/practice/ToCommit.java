77058ea9ded18676041747ee6e9a917b1712c93a
kiran <kiran@Kiran>
2024-03-08 11:13:40 +0530

    New package added

---
+++ GitHub/src/test/java/practice/ToCommit.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToCommit {
+
+	@Test
+	public void toCommitandPush() {
+		Reporter.log("Commiting", true);
+	}
+
+}
